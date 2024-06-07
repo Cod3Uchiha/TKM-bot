64f9d12f2e44190cabb19fdcbad1f4957dcb4ed1
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-06-07 18:39:03 +0200

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'Zokou-MD',
+      author: 'TKM--bot',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
