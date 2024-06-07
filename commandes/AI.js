2a31ec09164405f55faa739c9a8d31d4bf34bd83
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-06-07 16:08:11 +0200

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"bot",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"tkm",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
@@ -58,7 +58,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
       const response = await axios.get(`http://api.maher-zubair.tech/ai/photoleap?q=${image}`);
       
       const data = response.data;
-      let caption = '*powered by ZOKOU-MD*';
+      let caption = 'powered by TKM md';
       
       if (data.status == 200) {
         // Utiliser les données retournées par le service
