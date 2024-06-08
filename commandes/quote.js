6a88af4d8a85cd86783da1cfbed09068421768a9
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-06-08 09:10:26 +0200

    Update quote.js

--- commandes/quote.js
+++ commandes/quote.js
@@ -13,14 +13,14 @@ zokou({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) =
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Zokou-md               ║
+║   ⬡┃TKM bot┃⬡                ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Zokou-MD`);
+Powered by ⬡┃TKM bot┃⬡`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -33,14 +33,14 @@ Powered by Zokou-MD`);
         .then((response) => response.json())
         .then(async (quote) => {
           repondre(`╔══════════════════════════╗
-║   Zokou-md               ║
+║   ⬡┃TKM bot┃⬡                ║
 ╚══════════════════════════╝
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by Zokou-MD`);
+Powered by ⬡┃TKM bot┃⬡`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
