42a2bdd3ce7ad3dc314acbf644aef0596d103ff1
DannyAkintunde <doakintunde@outlook.com>
2024-06-20 03:59:45 +0100

    koyeb and render deploy

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const http = require('http');
 const fs = require('fs');
+const path = require("path");
 
-const page = ".media/site/index.html"
+const page = path.join(__dirname, "media", "site", "assets")
+
 
 function server(req,res) {
-  fs.readFile(page, (err,data) => {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not Found');
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    }
-  });
+  if (req.url === '/') {
+    fs.readFile(path.join(page, 'index.html'), (err,data) => {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/style.css'){
+    fs.readFile(path.join(page, "style.css"), (err,data) => {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      }
+    });
+  }
 }
 
 function start(port) {
