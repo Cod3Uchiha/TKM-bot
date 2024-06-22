4a1eccf14b9ccd36b78b24d588e31e38f4cd2ccc
DannyAkintunde <doakintunde@outlook.com>
2024-06-22 01:28:34 +0100

    server update

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const page = path.join(__dirname, "media", "site")
 
 
 function server(req,res) {
+  console.log(`server: GET '${req.url}'`);
   if (req.url === '/') {
     fs.readFile(path.join(page, 'index.html'), (err,data) => {
       if (err) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('Not Found');
+        console.log(`server: response 404 'Not Found'`);
       } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
+        console.log(`server: response 200 'index.html'`);
       }
     });
   } else if (req.url === '/style.css'){
@@ -21,9 +24,11 @@ function server(req,res) {
       if (err) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('Not Found');
+        console.log(`server: response 404 'Not Found'`);
       } else {
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.end(data);
+        console.log(`server: response 200 'style.css'`)
       }
     });
   }
