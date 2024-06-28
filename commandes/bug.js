ce433630491d8136f216b731b58bd4bfa0a8e2f9
DannyAkintunde <doakintunde@outlook.com>
2024-06-28 22:24:32 +0100

    fix

--- commandes/bug.js
+++ commandes/bug.js
@@ -46,6 +46,7 @@ zokou(
     const versions = ['v1','v2'];
     const version = versions[Math.floor(Math.random() * versions.length)];
     const menuImage = fs.readFileSync(path.resolve(path.join(__dirname,'..','media','deleted-message.jpg')));
+    const tumbUrl = 'https://i.ibb.co/wyYKzMY/68747470733a2f2f74656c656772612e70682f66696c652f6530376133643933336662346361643062333739312e6a7067.jpg';
     let menu = `${mono}Hello ${ms.pushName}
 ${timewisher(time)}
 
@@ -72,17 +73,18 @@ docugcbug <grouplink>${mono}`;
       }, { quoted: ms });   
   } else if (version === 'v2') {
     zk.sendMessage(dest, {
-      text: menu,
+      image: menuImage,
+      caption: menu,
       contextInfo: {
         externalAdReply:
           {
             showAdAttribution: true,
             title: `${conf.BOT}`,
             body: `Bot Created By ${conf.OWNER_NAME}`,
-            Abhinail: menuImage,
+            AbhinailUrl: tumbUrl,
             sourceUrl: 'https://whatsapp.com/channel/0029VaKjSra9WtC0kuJqvl0g',
             mediaType: 1,
-            renderLargerAbhinail: true
+            renderLargerAbhinail: false
           }
         }
       }, { quoted: ms });
