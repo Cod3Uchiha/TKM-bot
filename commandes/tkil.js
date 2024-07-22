a71cd923c4005352bf48184243830a20cb57b2d0
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-07-22 19:18:46 +0200

    Create tkil.js

---
+++ commandes/tkil.js
@@ -0,0 +1,30 @@
+const { zokou } = require("../framework/zokou");
+const conf = require("../set");
+
+const ios2 = "🌤️";
+const ios1 = "𝐋𝐎𝐑𝐃𒆜𝐓𝐊𝐌";
+const ngazap = "𝐬𝐚𝐲𝐬 🖕😎🖕 🔥";
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+zokou(
+  {
+    nomCom: "tkill",
+    categorie: "Mods",
+    reaction: "😈",
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre, superUser } = commandeOptions;
+    const limit = conf.TKILL_MESSAGE_LIMIT;
+
+    if (!superUser) {
+      repondre("You are not authorized to use this command!!!");
+      return;
+    } else {
+      for (let i = 0; i < limit; i++) {
+        attention(`${ios2}${ios1}${ngazap}`);
+        await sleep(1000);
+      }
+      repondre("*Sukses mengirim Bug Ke Tolong Jeda 3 Menit Yah*");
+    }
+  }
+);
